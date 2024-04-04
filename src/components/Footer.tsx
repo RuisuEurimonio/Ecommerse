1785cf58bab392749e4431ab5e96acc5d6851d86
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-04-03 20:39:41 -0500

    Se diseña el nav y footer responsive, TODO: implementar la navegacion en mobile y tablet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,58 +10,113 @@ type FooterProps ={
 const Footer: React.FC<FooterProps> = () => {
     return (
         <footer className="bg-red-mafer w-full">
-            <div className="w-[80%] m-auto">
-                <div className="flex justify-around py-7">
-                    <div className="flex items-center mx-5 gap-10">
-                        <Image 
+            <div className="w-[90%] m-auto
+                lg:w-[80%]
+            ">
+                <div className="flex flex-col justify-around py-5
+                    xl:flex-row xl:py-7
+                ">
+                    <div className="flex flex-col gap-1 justify-center items-center
+                        sm:flex-row sm:gap-3 xl:w-1/2
+                        xl:justify-center xl:gap-5 xl:mx-5 
+                    ">
+                        <Image className=""
                             src={logo}
                             alt={"Logo de Centro Ferretero Mafer S.A.S."}
-                            width={250}
+                            width={180}
                         />
-                        <p className="block max-w-[400px] mx-5 text-justify text-sm"> <span className="font-semibold">Centro Ferretero Mafer</span> es una entidad debidamente constituida, especializada en la venta de productos de ferretería en general y construcción, con el objeto de ofrecer un servicio integral a todos nuestros clientes, contamos con el personal humano mas idóneo, una infraestructura organizacional, la cual nos permite que el desarrollo de nuestro objeto social trascienda a nuestros clientes en una calidad de servicio, brindando una oportuna y efectiva atención, lo que garantiza los resultados mas confiables. </p>
+                        <p className="block mx-5 text-justify py-4 text-sm 
+                            xl:py-0
+                            2xl:text-base
+                        "> <span className="font-semibold">Centro Ferretero Mafer</span> es una entidad debidamente constituida, especializada en la venta de productos de ferretería en general y construcción, con el objeto de ofrecer un servicio integral a todos nuestros clientes, contamos con el personal humano mas idóneo, una infraestructura organizacional, la cual nos permite que el desarrollo de nuestro objeto social trascienda a nuestros clientes en una calidad de servicio, brindando una oportuna y efectiva atención, lo que garantiza los resultados mas confiables. </p>
                     </div>
-                    <div className="flex gap-8">
+                    <div className="flex gap-4 justify-around flex-wrap
+                        sm:flex-nowrap
+                        xl:gap-8 xl:justify-normal 
+                    ">
                         <ul className="text-black-mafer text-sm">
-                            <li className="font-bold text-xl"> Contactanos:  </li>
+                            <li className="font-bold text-base
+                                xl:text-lg
+                            "> Contactanos:  </li>
                             <li>
-                                <h3 className="pl-1 text-base font-semibold"> Ubicación: </h3> 
-                                <p className="pl-3"> Calle 12 # 15 - 30 / Carrera 15 # 12 - 61 </p>
-                                <p className="pl-3"> Bogotá Colombia. </p>
+                                <h3 className="pl-1 text-sm font-semibold
+                                    xl:text-base
+                                "> Ubicación: </h3> 
+                                <p className="pl-3 text-xs
+                                    xl:text-sm
+                                "> Calle 12 # 15 - 30 / Carrera 15 # 12 - 61 </p>
+                                <p className="pl-3 text-xs
+                                    xl:text-sm
+                                "> Bogotá Colombia. </p>
                             </li>
                             <li>
-                                <h3 className="pl-1 text-base font-semibold"> Correo: </h3> 
-                                <p className="pl-3">ventas@centroferreteromafer.com </p>
+                                <h3 className="pl-1 text-sm font-semibold
+                                    xl:text-base
+                                "> Correo: </h3> 
+                                <p className="pl-3 text-xs
+                                    xl:text-sm
+                                ">ventas@centroferreteromafer.com </p>
                             </li>
                             <li>
-                                <h3 className="pl-1 text-base font-semibold"> Telefono: </h3> 
-                                <p className="pl-3">PBX: 742 19 81 </p>
+                                <h3 className="pl-1 text-sm font-semibold
+                                    xl:text-base
+                                "> Telefono: </h3> 
+                                <p className="pl-3 text-xs
+                                    xl:text-sm
+                                ">PBX: 742 19 81 </p>
                             </li>
                             <li> 
-                                <h3 className="pl-1 text-base font-semibold"> Horario: </h3>
-                                <p className="pl-3"> Lunes - Viernes / 7:00 AM - 5:00 PM.</p>
-                                <p className="pl-3"> Sábado /7:00 AM - 4:00 PM. </p>
+                                <h3 className="pl-1 text-sm font-semibold
+                                    xl:text-base
+                                "> Horario: </h3>
+                                <p className="pl-3 text-xs
+                                    xl:text-sm
+                                "> Lunes - Viernes / 7:00 AM - 5:00 PM.</p>
+                                <p className="pl-3 text-xs
+                                    xl:text-sm
+                                "> Sábado /7:00 AM - 4:00 PM. </p>
                             </li>
                         </ul>
                         <ul className="text-black-mafer font-semibold">
-                            <li className="font-bold text-xl"> Tienda: </li>
-                            <li> <Link href="/productos"> Productos. </Link> </li>
-                            <li> <Link href="/"> Marcas. </Link> </li>
-                            <li> <Link href="/"> Categorias. </Link> </li>
-                            <li> <Link href="/"> Descuentos. </Link> </li>
-                            <li> <Link href="/"> JOSC. </Link> </li>
+                            <li className="font-bold text-base
+                                xl:text-lg"> Tienda: </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/productos"> Productos. </Link> </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/"> Marcas. </Link> </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/"> Categorias. </Link> </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/"> Descuentos. </Link> </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/"> JOSC. </Link> </li>
                         </ul> 
                         <ul className="text-black-mafer font-semibold">
-                            <li className="font-bold text-xl"> Información: </li>
-                            <li> <Link href="/about"> Sobre nosotros.  </Link> </li>
-                            <li> <Link href="/josc"> Nuestra marca. </Link> </li>
-                            <li> <Link href="/"> Medios de pago. </Link> </li>
+                            <li className="font-bold text-base
+                                xl:text-lg"> Información: </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/about"> Sobre nosotros.  </Link> </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/josc"> Nuestra marca. </Link> </li>
+                            <li className="pl-1 text-sm font-semibold
+                                xl:text-base
+                            "> <Link href="/"> Medios de pago. </Link> </li>
                         </ul>
                     </div>
                 </div>
                 <hr/>
                 <div className="relative py-3">
                     <h4 className="text-xs text-center"> © Centro Ferretero Mafer 2024. Todos los derechos reservados. Desarrollado por Ruisu's Software </h4>
-                    <ul className="absolute right-0 top-1/2 -translate-y-1/2 flex gap-5 text-xl">
+                    <ul className="justify-center flex gap-5  mt-3
+                        xl:absolute xl:right-0 xl:top-1/2 xl:-translate-y-1/2 xl:text-xl xl:justify-normal xl:mt-0
+                    ">
                         <li> <Link target="_blank" href="https://www.instagram.com/centro_ferretero_mafer_sas"> <span className="icon icon-instagram"/> </Link> </li>
                         <li> <Link target="_blank" href="https://www.facebook.com/CentroFerreteroMaferSAS"> <span className="icon icon-facebook"/> </Link> </li>
                         <li> <Link target="_blank" href="https://www.tiktok.com/@centrofmafer"> <span className="icon icon-tiktok"/> </Link> </li>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,12 @@ type navProps = {};
 
 const NavBar : React.FC<navProps> = () => {
     return (
-        <nav className="bg-red-mafer w-[100%]">
-            <div className="flex justify-between items-center max-w-[80%] m-auto">
-                <div className="m-2">
+        <nav className="bg-red-mafer w-[100%] p-2">
+            <div className="flex justify-between items-center w-full m-auto
+                xl:w-[80%]">
+                <div className="m-2 w-1/3
+                                lg:w-[288px]
+                    ">
                     <Image
                         src={logoGif}
                         alt="Logo en movimiento de Centro Ferretero Mafer"
@@ -18,16 +21,35 @@ const NavBar : React.FC<navProps> = () => {
                         unoptimized={false}
                     />            
                 </div>
-                <div className="w-[30%]">
-                    <form className="w-[100%] h-10">
-                        <input className="w-4/5 rounded-l-3xl h-full pl-5 outline-none" placeholder="Buscar">
-                        </input>
-                        <button className="w-1/5 bg-white-mafer rounded-r-3xl h-full border-l-2 border-b-red-mafer ">
+                <div className="
+                    w-[50%]
+                    md:w-[30%]
+                ">
+                    <form className="w-[90%] m-auto flex 
+                        md:h-7
+                        xl:h-10 xl:w-full
+                        ">
+                        <input className="w-4/5 rounded-l-3xl pl-5 outline-none text-base
+                            md:h-full 
+                        " placeholder="Buscar"/>
+                        <button className="w-1/5 bg-white-mafer rounded-r-3xl h-full border-l-2 text-base
+                            md:h-full 
+                        " name="Buscar">
                             <span className="icon icon-search"></span>
                         </button>
                     </form>
                 </div>
-                <ul className="flex text-black-mafer gap-5 font-bold">
+                <button className="
+                    mr-3 text-xl
+                    md:hidden
+                ">
+                        <span className="icon icon-bars" />
+                </button>
+                <ul className="
+                  text-black-mafer text-center m-5 font-bold items-center gap-1 text-xs hidden
+                    lg:gap-3 lg:m-2 lg:text-sm 
+                    md:flex
+                ">
                     <li><Link href="" className="hover:text-black-mafer hover:underline hover:underline-offset-8"> Inicio </Link></li>
                     <li><Link href="" className="hover:text-black-mafer hover:underline hover:underline-offset-8"> Productos </Link></li>
                     <li><Link href="" className="hover:text-black-mafer hover:underline hover:underline-offset-8"> Descuentos </Link></li>
@@ -35,11 +57,14 @@ const NavBar : React.FC<navProps> = () => {
                     <li><Link href="" className="hover:text-black-mafer hover:underline hover:underline-offset-8"> Eventos </Link></li>
                     <li><Link href="" className="hover:text-black-mafer hover:underline hover:underline-offset-8"> Sobre Nosotros </Link></li>
                 </ul>
-                <div className="text-xl">
-                    <button className="h-10 border-r-[0.5px]">
+                <div className="text-base text-nowrap h-7 hidden
+                    xl:text-xl xl:h-full
+                    md:block
+                ">
+                    <button className="h-full border-r-[0.5px]">
                         <span className=" h-full px-3 icon icon-cart before:content-['\e906'] before:text-black-mafer"></span>
                     </button>
-                    <button className="h-10 border-l-[0.5px]">
+                    <button className="h-full border-l-[0.5px]">
                         <span className="h-full px-3 icon icon-user before:content-['\e907'] before:text-black-mafer"></span>
                     </button>
                 </div>
