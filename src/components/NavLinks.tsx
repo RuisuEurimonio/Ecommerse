b0b7daf1eb605da5111c336acb331029b876cc23
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-04-16 18:17:36 -0500

    Se cambia diseño en el navlink para versiones moviles, dejando solo la franja roja y lo demas blanco como los filtros

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -36,41 +36,45 @@ const NavLinks:React.FC<NavLinksProps> = () => {
                 onClick={()=>{setActive(!active)}}>
                     <span className="icon icon-bars" />
             </button>
-            <div className={`fixed right-0 top-0 w-[320px] px-6 h-screen bg-red-mafer border-2 transition duration-500 z-40
+            <div className={`fixed right-0 top-0 w-[320px] h-screen bg-light-white-mafer border-2 transition duration-500 z-40
                 md:static md:translate-x-0 md:px-0 md:py-0 md:h-auto md:border-0 md:w-auto
                 ${active ? "translate-x-0" : "translate-x-full"}
             `}
             >
-                <div className="flex w-full flex-row-reverse">
-                    <button className="relative right-0 p-2 text-2xl
-                    md:hidden"
-                        onClick={()=>{setActive(!active)}}
-                    >
-                        <span className="icon icon-xmark"/>
-                    </button>
+                <div className=" w-full bg-red-mafer">
+                    <div className="flex w-11/12 flex-row-reverse">
+                        <button className="relative right-0 px-2 py-1 text-2xl
+                        md:hidden"
+                            onClick={()=>{setActive(!active)}}
+                        >
+                            <span className="icon icon-xmark"/>
+                        </button>
+                    </div>
                 </div>
-                <h3 className="md:hidden font-bold text-lg"> Menu: </h3>
-                <ul className="text-black-mafer text-center m-5 font-bold items-center gap-3 text-lg flex-col flex
-                    md:flex md:flex-row md:text-sm
-                    lg:gap-3 lg:m-2 
-                    2xl:text-base
-                ">
-                    {links.map((link:LinkProps)=>{
-                        const style = (pathName==link.href) ? "font-extrabold underline underline-offset-8" : "font-bold";
-                    return(
-                        <li key={link.nameLink}><Link href={link.href} className={`hover:text-black-mafer font- hover:underline hover:underline-offset-8 ${style}`}> {link.nameLink} </Link></li>
-                    )}
-                    )}
-                </ul>
-                <hr className="md:hidden"/>
-                <ul className="md:hidden text-lg my-4 font-semibold">
-                    <li> <Link href="/">Ingresar. </Link>  </li>
-                    <li> <Link href="/">Mi cuenta </Link>  </li>
-                </ul>
-                <hr className="md:hidden"/>
-                <ul className="md:hidden text-lg my-4 font-semibold">
-                    <li> <Link href="/"> Carrito </Link>  </li>
-                </ul>
+                <div className="px-6 py-2">
+                    <h3 className="md:hidden font-bold text-lg"> Menu: </h3>
+                    <ul className="text-black-mafer text-center m-5 font-bold items-center gap-3 text-lg flex-col flex
+                        md:flex md:flex-row md:text-sm
+                        lg:gap-3 lg:m-2 
+                        2xl:text-base
+                    ">
+                        {links.map((link:LinkProps)=>{
+                            const style = (pathName==link.href) ? "font-extrabold underline underline-offset-8" : "font-bold";
+                        return(
+                            <li key={link.nameLink}><Link href={link.href} className={`hover:text-black-mafer font- hover:underline hover:underline-offset-8 ${style}`}> {link.nameLink} </Link></li>
+                        )}
+                        )}
+                    </ul>
+                    <hr className="md:hidden"/>
+                    <ul className="md:hidden text-lg my-4 font-semibold">
+                        <li> <Link href="/">Ingresar. </Link>  </li>
+                        <li> <Link href="/">Mi cuenta </Link>  </li>
+                    </ul>
+                    <hr className="md:hidden"/>
+                    <ul className="md:hidden text-lg my-4 font-semibold">
+                        <li> <Link href="/"> Carrito </Link>  </li>
+                    </ul>
+                </div>
             </div>
         </>
     )
