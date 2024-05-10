6a36aead52e1658523e0ef6dcc38a1b991f588c4
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-05-10 18:49:26 -0500

    Se termina de agregar el componente de metodos de pago

--- src/components/PayMethods.tsx
+++ src/components/PayMethods.tsx
@@ -7,8 +7,8 @@ const data = fakePayMethods;
 
 const PayMethods : React.FC<PayMethodsProps> = () => {
     return(
-        <div>
-            <h1> Metodos de pagos seguros. </h1>
+        <div className="my-2">
+            <h1 className="text-blue-mafer font-bold text-lg text-center my-1"> Metodos de pagos seguros. </h1>
             <CarouselSimple data={data} intervalMove={5000} />
         </div>
     )
