06a4bbe11d20506643336be434f3ba7467239ca1
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-05-21 22:04:38 -0500

    Se estila el boton de envio y se agrega validación de inputs

--- src/components/FormUser.tsx
+++ src/components/FormUser.tsx
@@ -1,111 +1,188 @@
-"use client"
+"use client";
 
-import {useForm} from "react-hook-form"
+import { userSchequema } from "@/utils/Schemas/userSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
 
 type FormUserProps = {
-    className?: string
-}
+    className?: string;
+};
+
+type formProps = z.infer<typeof userSchequema>;
 
 const document = [
-    {nombre : "Tarjeta de identidad", abreviatura: "TI"},
-    {nombre : "Cedula de Ciudadania", abreviatura: "CC"},
-    {nombre : "Cedula de extranjeria", abreviatura: "CED"}
-]
+    { nombre: "Tarjeta de identidad", abreviatura: "TI" },
+    { nombre: "Cedula de Ciudadania", abreviatura: "CC" },
+    { nombre: "Cedula de extranjeria", abreviatura: "CED" },
+];
 
-const FormUser : React.FC<FormUserProps> = ({className}) => {
+const FormUser: React.FC<FormUserProps> = ({ className }) => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<formProps>({
+        resolver: zodResolver(userSchequema),
+    });
 
-    const {register} = useForm();
+    const onSubmit : SubmitHandler<formProps> = (data) => {
+        console.log(data);
+    }
 
-    return(
-        <form action="" className={className}>
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={className}
+        >
             <div className="flex flex-col gap-4">
                 <h3 className="font-bold text-lg"> Detalles. </h3>
                 <div className="inline ml-5">
                     <label htmlFor="numberDocument">
                         <p className="inline-block">Documento:</p>
-                        <select className="border" 
+                        <select
+                            className="border rounded-sm ml-2"
                             {...register("TypeDocument")}
                         >
                             {document.map((item) => (
-                                <option key={item.abreviatura} value={item.nombre}> {item.abreviatura} </option>
+                                <option
+                                    key={item.abreviatura}
+                                    value={item.nombre}
+                                >
+                                    {" "}
+                                    {item.abreviatura}{" "}
+                                </option>
                             ))}
                         </select>
-                        <input id="numberDocument" type="text" className="border"
-                        {...register("numberDocument")}
+                        <input
+                            id="numberDocument"
+                            type="text"
+                            className="border rounded-sm"
+                            {...register("numberDocument")}
                         />
                     </label>
+                    {errors.TypeDocument && (
+                        <p> {errors.TypeDocument.message} </p>
+                    )}
+                    {errors.numberDocument && (
+                        <p> {errors.numberDocument.message} </p>
+                    )}
                 </div>
                 <div className="ml-5">
                     <label htmlFor="names">
                         Nombres:
-                        <input id="names" type="text" className="border"
+                        <input
+                            id="names"
+                            type="text"
+                            className="border rounded-sm ml-2"
                             {...register("names")}
                         />
                     </label>
+                    {errors.names && <p> {errors.names.message} </p>}
                 </div>
                 <div className="ml-5">
                     <label htmlFor="lastNames">
                         Apellidos:
-                        <input id="lastNames" type="text" className="border"
+                        <input
+                            id="lastNames"
+                            type="text"
+                            className="border rounded-sm ml-2"
                             {...register("lastNames")}
                         />
                     </label>
+                    {errors.lastNames && <p> {errors.lastNames.message} </p>}
                 </div>
                 <div className="ml-5">
                     <label htmlFor="numberPhone">
                         Teléfono:
-                        <input id="numberPhone" type="text" className="border" 
+                        <input
+                            id="numberPhone"
+                            type="text"
+                            className="border rounded-sm ml-2"
                             {...register("numberPhone")}
                         />
-                    </label>    
+                    </label>
+                    {errors.numberPhone && (
+                        <p> {errors.numberPhone.message} </p>
+                    )}
                 </div>
                 <div className="ml-5">
                     <label htmlFor="address">
                         Dirección:
-                        <input id="address" type="text" className="border" 
+                        <input
+                            id="address"
+                            type="text"
+                            className="border rounded-sm ml-2"
                             {...register("address")}
                         />
                     </label>
+                    {errors.address && <p> {errors.address.message} </p>}
                 </div>
                 <h3 className="font-bold text-lg"> Dirección de correo. </h3>
                 <div className="ml-5">
                     <label htmlFor="email">
                         Correo:
-                        <input id="email" type="email" className="border" 
+                        <input
+                            id="email"
+                            type="email"
+                            className="border rounded-sm ml-2"
                             {...register("email")}
                         />
                     </label>
+                    {errors.email && <p> {errors.email.message} </p>}
                 </div>
                 <h3 className="font-bold text-lg"> Seguridad. </h3>
                 <div className="ml-5">
                     <label htmlFor="password">
                         Contraseña actual:
-                        <input id="password" type="password" className="border" 
+                        <input
+                            id="password"
+                            type="password"
+                            className="border rounded-sm ml-2"
                             {...register("password")}
                         />
                     </label>
+                    {errors.password && <p> {errors.password.message} </p>}
                 </div>
                 <div className="ml-5">
                     <label htmlFor="newPassword">
                         Nueva contraseña:
-                        <input id="newPassword" type="password" className="border" 
+                        <input
+                            id="newPassword"
+                            type="password"
+                            className="border rounded-sm ml-2"
                             {...register("newPassword")}
                         />
                     </label>
+                    {errors.newPassword && (
+                        <p> {errors.newPassword.message} </p>
+                    )}
                 </div>
                 <div className="ml-5">
                     <label htmlFor="repeatPassword">
                         Confirmar nueva contraseña:
-                        <input id="repeatPassword" type="password" className="border" 
+                        <input
+                            id="repeatPassword"
+                            type="password"
+                            className="border rounded-sm ml-2"
                             {...register("repeatPassword")}
                         />
                     </label>
+                    {errors.repeatPassword && (
+                        <p> {errors.repeatPassword.message} </p>
+                    )}
                 </div>
                 <h3 className="font-bold text-lg"> Pagos. </h3>
             </div>
+            <input
+                type="submit"
+                value="Enviar"
+                className="bg-blue-mafer text-white px-2 py-1 rounded-sm text-right cursor-pointer"
+            />
+            {JSON.stringify(watch(), null, 2)}
         </form>
-    )
-
-}
+    );
+};
 
 export default FormUser;
---
+++ src/utils/Schemas/userSchema.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+export const userSchequema = z
+    .object({
+        TypeDocument: z.enum([
+            "Tarjeta de identidad",
+            "Cedula de Ciudadania",
+            "Cedula de extranjeria",
+        ]),
+        numberDocument: z
+            .string()
+            .refine((number) => !isNaN(parseInt(number)), {
+                message: "El documento debe tener solo números",
+            }),
+        names: z
+            .string()
+            .min(3, "Ingrese un nombre válido")
+            .max(30, "Ingrese un nombre válido"),
+        lastNames: z
+            .string()
+            .min(3, "Ingrese un apellido válido")
+            .max(30, "Ingrese un apellido válido"),
+        numberPhone: z
+            .string()
+            .refine((number) => !isNaN(parseInt(number)), {
+                message: "Debe ingresar solo números",
+            }),
+        address: z
+            .string()
+            .min(13, "Ingrese una dirección válida")
+            .max(45, "Ingrese una dirección válida"),
+        email: z.string().email({ message: "ingrese un correo valido" }),
+        password: z
+            .string()
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                "La contraseña debe tener 1 letra minúscula, mayúscula, número y un caracter especial"
+            ),
+        newPassword: z
+            .string()
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                "La contraseña debe tener 1 letra minúscula, mayúscula, número y un caracter especial"
+            ),
+        repeatPassword: z
+            .string()
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                "La contraseña debe tener 1 letra minúscula, mayúscula, número y un caracter especial"
+            ),
+    })
+    .refine((data) => data.newPassword === data.repeatPassword, {
+        message: "Las contraseñas deben ser iguales.",
+        path: ["repeatPassword"],
+    });
