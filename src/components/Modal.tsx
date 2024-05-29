57dc75124b2a6df548e240d17d003c7069ef010e
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-05-28 20:01:19 -0500

    Se agrega el componente de formulario al modal, TODO: hacer modificaciones en este componente

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import FormUser from "./FormUser";
 
 type ModalProps = {
     type?: string,
@@ -11,16 +11,21 @@ type ModalProps = {
 const Modal : React.FC<ModalProps> = ({type, openCloseModal, state}) => {
 
     return (
-        <div className="fixed bg-black/20 h-screen w-screen z-50 top-0 left-0 flex items-center justify-center"
+        <div className="fixed bg-black/20 h-screen w-screen z-50 top-0 left-0"
             style={{display: (state) ? "flex" : "none"}}
             onClick={openCloseModal}
         >
-            <div className="w-4/5 m-auto">
+            <div className="w-11/12 h-5/6 m-auto bg-white rounded-sm shadow-md flex flex-col items-center justify-center py-5 overflow-y-auto
+                lg:w-1/2 lg:overflow-x-hidden    
+            "
+                onClick={(e)=>{e.stopPropagation()}}
+            >
                 {type === "post" ?
-                (<h2> Agregar usuario. </h2>)
+                (<h2 className="font-bold text-blue-mafer text-xl m-2"> Agregar usuario. </h2>)
                 :
-                (<h2> Actualizar usuario. </h2>)
+                (<h2 className="font-bold text-blue-mafer text-xl m-2"> Actualizar usuario. </h2>)
             }
+                <FormUser className="w-11/12 h-full"/>
             </div>
         </div>
     )
