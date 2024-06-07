e4a4a716eaef87f3dd9df78f670ff35cc544db06
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-06-07 18:31:30 -0500

    Organización

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,14 +1,17 @@
-import HistoryNavigation from "@/components/HistoryNavigation";
-import fakeNewsletters from "@/utils/json/fakeNewsletters.json"
 import Image from "next/image";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
-const Event = ({params}:any) => {
+import HistoryNavigation from "@/components/HistoryNavigation";
 
-    const dataEvent = fakeNewsletters.find((item => item.id === parseInt(params.id)))
+import fakeNewsletters from "@/utils/json/fakeNewsletters.json"
+
+const Event = ({params}:Params) => {
+
+    const dataEvent = fakeNewsletters.find((item => item.id === parseInt(params.id))) //TODO delete this when we implement fetch data
 
     return(
         <div className="w-11/12 m-auto
-        md:w-4/5">
+            md:w-4/5">
             <HistoryNavigation items={[{names:"Eventos",url:"/events"},{names:dataEvent?.title || "Not found", url:"#"}]}/>
             <p className="text-end text-sm my-1"> {dataEvent?.date} </p>
             <h2 className="my-2 text-lg text-blue-mafer font-bold text-center"> {dataEvent?.title || "Not found"} </h2>
