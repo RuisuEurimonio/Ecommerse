4f45b40ce8239671b1387e8c5a788912969131a0
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-06-08 10:40:10 -0500

    Organización y limpieza

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,8 +1,9 @@
+import React from "react";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
 import { CardProductProps } from "@/types/Props";
-import imageNotFound from "@/assets/img/imageNotFound.jpg";
 
 type CardItemProps = {
     item: CardProductProps;
@@ -20,13 +21,8 @@ const CardItem: React.FC<CardItemProps> = ({
             <div className="relative">
                 {discount && (
                     <React.Fragment>
-                        <span className="absolute top-0 right-0 border-[1.5rem] w-0 h-0 border-red-mafer border-l-transparent border-b-transparent">
-                            {" "}
-                        </span>
-                        <span className="absolute top-0.8 right-1 font-bold text-lg">
-                            {" "}
-                            %{" "}
-                        </span>
+                        <span className="absolute top-0 right-0 border-[1.5rem] w-0 h-0 border-red-mafer border-l-transparent border-b-transparent"/>
+                        <span className="absolute top-0.8 right-1 font-bold text-lg"> % </span>
                     </React.Fragment>
                 )}
                 <div className="w-full">
@@ -41,54 +37,43 @@ const CardItem: React.FC<CardItemProps> = ({
                 </div>
                 <p
                     className="text-sm mt-2 line-clamp-1
-                    xl:text-base
-                "
+                    xl:text-base"
                 >
-                    {" "}
-                    {item.id} {item.clasificacion}{" "}
+                    {item.id} {item.clasificacion}
                 </p>
                 <p
                     className="text-base font-semibold
-                    xl:text-lg
-                "
+                    xl:text-lg"
                 >
-                    {" "}
-                    {item.nombre}{" "}
+                    {item.nombre}
                 </p>
                 <p
                     className="text-ellipsis overflow-hidden text-xs line-clamp-3
-                    xl:text-sm
-                "
+                    xl:text-sm"
                 >
-                    {" "}
-                    {item.descripcion}{" "}
+                    {item.descripcion}
                 </p>
             </div>
             <div
                 className="flex text-sm flex-col-reverse items-center m-1
                 lg:flex-row lg:gap-2 lg:text-center
                 xl:text-lg
-                2xl:text-base
-            "
+                2xl:text-base"
             >
                 <button
                     className="bg-blue-mafer/80 hover:bg-blue-mafer text-white-mafer py-1 px-2 rounded-md transition
                     lg:text-xs
-                    2xl:text-sm
-                "
+                    2xl:text-sm"
                 >
-                    {" "}
-                    Agregar al carrito{" "}
+                    Agregar al carrito
                 </button>
                 <Link
                     href={`/${link}/${item.id}`}
                     className="hover:underline
                     lg:text-xs
-                    2xl:text-sm
-                "
+                    2xl:text-sm"
                 >
-                    {" "}
-                    Más información.{" "}
+                    Más información.
                 </Link>
             </div>
         </div>
