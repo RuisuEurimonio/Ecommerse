c72e02cf153a97c372a916957e9235b98b7b655c
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-07-03 17:42:38 -0500

    Se termino el diseño del componente de login

--- src/app/authForm/page.tsx
+++ src/app/authForm/page.tsx
@@ -24,36 +24,40 @@ const AuthForm : React.FC<AuthFormProps> = () => {
     ]
 
     return (
-        <div className="w-11/12 min-h-[70vh] mx-auto my-5 flex flex-col-reverse justify-center border rounded-sm divide-y divide-y-reverse"> 
-            <div className="bg-light-white-mafer p-3">
-                <h2 className="font-bold text-center text-xl"> Iniciar sesión. </h2>
-                <Form 
-                    dataName="Iniciar sesión"
-                    schequema={authFormLogin}
-                    inputsList={inputsList}
-                    modal>
-                <div className="flex items-center flex-col my-4">
-                    <label htmlFor="showPassword">
-                        <input type="checkbox" id="showPassword"/>
-                        Mostrar contraseña.
-                    </label>
-                    <label htmlFor="saveSession">
-                        <input type="checkbox" id="saveSession"/>
-                        Mantener sesión.
-                    </label>
-                    <p className="text-center text-sm mt-2"> ¿No tienes una cuenta? <button className=""> Registrate aquí. </button> </p>
-                    <p className="text-center text-sm"> ¿Olvidaste la contraseña? <Link href=""> Recuperar contraseña. </Link> </p>
+        <div className="w-11/12 min-h-[70vh] mx-auto flex items-center justify-center"> 
+            <div className="flex flex-col-reverse justify-center my-5 border rounded-sm divide-y divide-y-reverse
+                            md:flex-row
+            ">
+                <div className="bg-light-white-mafer p-3 flex justify-center flex-col w-full">
+                    <h2 className="font-bold text-center text-xl"> Iniciar sesión. </h2>
+                    <Form 
+                        dataName="Iniciar sesión"
+                        schequema={authFormLogin}
+                        inputsList={inputsList}
+                        modal>
+                    <div className="flex items-center flex-col my-4">
+                        <label htmlFor="showPassword">
+                            <input type="checkbox" id="showPassword"/>
+                            Mostrar contraseña.
+                        </label>
+                        <label htmlFor="saveSession">
+                            <input type="checkbox" id="saveSession"/>
+                            Mantener sesión.
+                        </label>
+                        <p className="text-center text-sm mt-2"> ¿No tienes una cuenta? <button className="font-bold underline text-blue-700 text-center"> Registrate aquí. </button> </p>
+                        <p className="text-center text-sm"> ¿Olvidaste la contraseña? <Link href="" className="font-bold underline text-blue-700 text-center"> Recuperar contraseña. </Link> </p>
+                    </div>
+                    </Form>
                 </div>
-                </Form>
-            </div>
-            <div className="bg-slate-900 h-full flex justify-center flex-col gap-3">
-                <h2 className="text-center text-xl font-bold text-white"> Bienvenido a nuestro espacio de clientes. </h2>
-                <div className="flex items-center justify-center"><Image src={image} alt="" width={150} height={150} className=""/></div>
-                <ul className="flex w-3/4 mx-auto my-2 justify-around text-white text-2xl">
-                    <li><span className="icon icon-truck"></span></li>
-                    <li><span className="icon icon-ranking"></span></li>
-                    <li><span className="icon icon-global"></span></li>
-                </ul>
+                <div className="bg-slate-900 flex justify-center flex-col gap-3 w-full">
+                    <h2 className="text-center text-xl font-bold text-white"> Bienvenido a nuestro espacio de clientes. </h2>
+                    <div className="flex items-center justify-center"><Image src={image} alt="" width={150} height={150} className=""/></div>
+                    <ul className="flex w-3/4 mx-auto my-2 justify-around text-white text-2xl">
+                        <li><span className="icon icon-truck"></span></li>
+                        <li><span className="icon icon-ranking"></span></li>
+                        <li><span className="icon icon-global"></span></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
