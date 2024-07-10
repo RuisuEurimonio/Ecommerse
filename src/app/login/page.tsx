9b5e4d78985315f050b035c3fd2f2e617ca10b1d
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-07-10 18:25:29 -0500

    Se ajusta el estilo de la página de inicio de sesión

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import Form from "@/components/Form";
 import { authFormLogin } from "@/utils/Schemas/authFormLogin";
 import Link from "next/link";
-import image from "@/assets/img/imageNotFound.jpg"
+import image from "@/assets/img/login.jpg"
 import Image from "next/image";
 import { useState } from "react";
 
@@ -39,8 +39,8 @@ const AuthForm : React.FC<AuthFormProps> = () => {
             <div className="flex flex-col-reverse justify-center my-5 border rounded-sm divide-y divide-y-reverse
                             md:flex-row
             ">
-                <div className="bg-light-white-mafer p-3 flex justify-center flex-col w-full">
-                    <h2 className="font-bold text-center text-xl mb-4"> Iniciar sesión. </h2>
+                <div className="bg-principal-color/5 p-3 flex justify-center flex-col w-full">
+                    <h2 className="font-bold text-center text-xl mb-4 text-fifth-color"> Iniciar sesión. </h2>
                     <Form 
                         dataName="Iniciar sesión"
                         schequema={authFormLogin}
@@ -56,14 +56,19 @@ const AuthForm : React.FC<AuthFormProps> = () => {
                             <input type="checkbox" id="saveSession"/>
                             Mantener sesión.
                         </label>
-                        <p className="text-center text-sm mt-2"> ¿No tienes una cuenta? <Link href="/register" className="font-bold underline text-blue-700 text-center"> Registrate aquí. </Link> </p>
-                        <p className="text-center text-sm"> ¿Olvidaste la contraseña? <Link href="/" className="font-bold underline text-blue-700 text-center"> Recuperar contraseña. </Link> </p>
+                        <p className="text-center text-sm mt-2"> ¿No tienes una cuenta? <Link href="/register" className="font-bold underline text-fifth-color text-center"> Registrate aquí. </Link> </p>
+                        <p className="text-center text-sm"> ¿Olvidaste la contraseña? <Link href="/" className="font-bold underline text-fifth-color text-center"> Recuperar contraseña. </Link> </p>
                     </div>
                     </Form>
                 </div>
-                <div className="bg-blue-mafer flex justify-center flex-col gap-3 w-full">
+                <div className="bg-secondary-color flex justify-center flex-col gap-3 w-full py-5">
                     <h2 className="text-center text-xl font-bold text-white"> Bienvenido a nuestro espacio de clientes. </h2>
-                    <div className="flex items-center justify-center"><Image src={image} alt="" width={150} height={150} className=""/></div>
+                    <div className="flex items-center justify-center">
+                        <Image src={image} alt="" 
+                        width={4272} 
+                        height={2848} 
+                        className="w-4/5 h-64 rounded-lg object-cover"/>
+                    </div>
                     <ul className="flex w-3/4 mx-auto my-2 justify-around text-white text-2xl">
                         <li><span className="icon icon-truck"></span></li>
                         <li><span className="icon icon-ranking"></span></li>
