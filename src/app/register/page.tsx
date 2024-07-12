796764bacbd05e6fa8a9a744fa62a6fb2007b606
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-07-11 21:12:49 -0500

    Ajuste de estilo en el componente de register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@
 
 import Form from "@/components/Form";
 import Link from "next/link";
-import image from "@/assets/img/imageNotFound.jpg"
+import image from "@/assets/img/register.jpg"
 import Image from "next/image";
 import { useState } from "react";
 import { AuthFormRegister } from "@/utils/Schemas/authFormRegister";
@@ -47,8 +47,8 @@ const AuthForm : React.FC<AuthFormProps> = () => {
                             md:flex-row-reverse md:w-10/12
                             xl:w-8/12
             ">
-                <div className="bg-light-white-mafer p-3 flex justify-center flex-col w-full">
-                    <h2 className="text-blue-mafer font-bold text-center text-xl mb-4"> Crear cuenta. </h2>
+                <div className="bg-third-color p-3 flex justify-center flex-col w-full">
+                    <h2 className="text-fifth-color font-bold text-center text-xl mb-4"> Crear cuenta. </h2>
                     <Form 
                         dataName="Iniciar sesión"
                         schequema={AuthFormRegister}
@@ -60,14 +60,16 @@ const AuthForm : React.FC<AuthFormProps> = () => {
                             <input type="checkbox" id="showPassword" onClick={onChange}/>
                             Mostrar contraseña.
                         </label>
-                        <p className="text-center text-sm mt-2"> ¿Ya tienes una cuenta? <Link href="/login" className="font-bold underline text-blue-700 text-center"> Ingresa aquí. </Link> </p>
+                        <p className="text-center text-sm mt-2"> ¿Ya tienes una cuenta? <Link href="/login" className="font-bold underline text-fifth-color text-center"> Ingresa aquí. </Link> </p>
                     </div>
                     </Form>
                 </div>
-                <div className="bg-blue-mafer flex justify-evenly flex-col gap-3 w-full">
-                    <h2 className="text-center text-xl font-bold text-white"> Beneficios. </h2>
-                    <div className="flex items-center justify-center"><Image src={image} alt="" width={350} height={350} className=""/></div>
-                    <ul className="flex w-3/4 mx-auto my-2 justify-around text-white text-2xl">
+                <div className="bg-secondary-color flex justify-evenly flex-col gap-3 w-full rounded-l-sm">
+                    <h2 className="text-center text-xl font-bold text-third-color"> Beneficios. </h2>
+                    <div className="flex items-center justify-center">
+                        <Image src={image} alt="" width={5472} height={3648} className="rounded-sm w-4/5 h-auto"/>
+                    </div>
+                    <ul className="flex w-3/4 mx-auto my-2 justify-around text-third-color text-2xl">
                         <li><span className="icon icon-truck"></span></li>
                         <li><span className="icon icon-ranking"></span></li>
                         <li><span className="icon icon-global"></span></li>
